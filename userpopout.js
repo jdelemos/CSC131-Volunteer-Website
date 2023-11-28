539e5f2d9fe7095e8bc023b9f57624c8b4c3d2ff
Bleepblop69 <75205610+Bleepblop69@users.noreply.github.com>
2023-11-28 00:38:07 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -1,6 +1,8 @@
 function checkbutton() {
+    // Retrieve the 'loggedIn' value from localStorage
     const loggedIn = localStorage.getItem('loggedIn');
     
+    // Check if the user is logged in
     if (loggedIn === 'true') {
         alert('User is logged in!');
 
@@ -11,12 +13,23 @@ function checkbutton() {
         hiddenTitles.forEach(title => {
             title.style.display = 'block';
         });
+
+        // Add a click event listener to the document
+        document.addEventListener('click', function() {
+            // Change the display status of each hidden title to "none" when any click occurs
+            hiddenTitles.forEach(title => {
+                title.style.display = 'none';
+            });
+
+            // Remove the click event listener after it's triggered once (optional)
+            document.removeEventListener('click', arguments.callee);
+        });
     } else {
+        // If the user is not logged in, display an alert and redirect to the login page
         alert('User is not logged in.');
         window.location.href = 'login.html';
     }
 }
-
 
 
 

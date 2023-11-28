fa39cffb130a50e689f70f3e072eeea7125464dd
kbhypno <101908689+kbhypno@users.noreply.github.com>
2023-11-27 22:35:18 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -1,28 +1,34 @@
-var titlesVisible = false;
-function handleClientLoad() {
-    gapi.load('auth2', initSignIn);
+//var titlesVisible = false;
+
+
+ //   var titles = document.querySelectorAll('.header .title.hidden');
+  //  titles.forEach(function (title) {
+   //     if (titlesVisible) {
+    //        title.style.display = 'none';
+    //    } else {
+    //        title.style.display = 'block';
+    //    }
+   // });
+
+   // titlesVisible = !titlesVisible;
+//} else {
+//    window.location.href = 'login.html';
+//}
+//}
+
+//document.getElementById('userCircle').addEventListener('click', toggleTitles);
+
+function checkLogin() {
+    const loggedIn = localStorage.getItem('loggedIn');
+
+    if (loggedIn === 'true') {
+        alert('User is logged in!');
+    } else {
+        alert('User is not logged in.');
+    }
 }
-
-function toggleTitles() {
-    console.log('Toggle titles function called');
-
-    var isUserSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
-
-    if( isUserSignedIn ) {
-    var titles = document.querySelectorAll('.header .title.hidden');
-    titles.forEach(function (title) {
-        if (titlesVisible) {
-            title.style.display = 'none';
-        } else {
-            title.style.display = 'block';
-        }
-    });
-
-    titlesVisible = !titlesVisible;
-} else {
-    window.location.href = 'login.html';
-}
-}
-
-handleClientLoad();
-document.getElementById('userCircle').addEventListener('click', toggleTitles);
+document.addEventListener('DOMContentLoaded', function() {
+    // Call the function to handle login state when the page loads
+    handleLoginState();
+});
+</script>

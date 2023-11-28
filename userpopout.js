31c9f2f916579bc97e0e91d889ce0d12d4c6a87e
Bleepblop69 <75205610+Bleepblop69@users.noreply.github.com>
2023-11-28 00:53:14 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -2,32 +2,27 @@ function checkbutton() {
     // Retrieve the 'loggedIn' value from localStorage
     const loggedIn = localStorage.getItem('loggedIn');
     
-    // Get all elements with the class "title-hidden"
-    const hiddenTitles = document.querySelectorAll('.title-hidden');
-
     // Check if the user is logged in
     if (loggedIn === 'true') {
         alert('User is logged in!');
 
-        // Initially hide the elements
+        // Get all elements with the class "title-hidden"
+        const hiddenTitles = document.querySelectorAll('.title-hidden');
+
+        // Change the display status of each hidden title to "block"
         hiddenTitles.forEach(title => {
-            title.style.display = 'none';
+            title.style.display = 'block';
         });
 
         // Add a click event listener to the document
-        document.addEventListener('click', function toggleTitles(event) {
-            // Check if the click target is the button
-            if (event.target.classList.contains('circle')) {
-                // Toggle the display status of each hidden title
-                hiddenTitles.forEach(title => {
-                    title.style.display = (title.style.display === 'none' || title.style.display === '') ? 'block' : 'none';
-                });
-            } else {
-                // If the click is anywhere else on the page, hide the elements
-                hiddenTitles.forEach(title => {
-                    title.style.display = 'none';
-                });
-            }
+        document.addEventListener('click', function hideTitles() {
+            // Change the display status of each hidden title to "none" when any click occurs
+            hiddenTitles.forEach(title => {
+                title.style.display = 'none';
+            });
+
+            // Remove the click event listener after it's triggered (optional, depending on your needs)
+            document.removeEventListener('click', hideTitles);
         });
     } else {
         // If the user is not logged in, display an alert and redirect to the login page

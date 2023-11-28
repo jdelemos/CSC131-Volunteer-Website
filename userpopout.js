d6a436164bb91485a743e8a0b07c2665a26493ce
kbhypno <101908689+kbhypno@users.noreply.github.com>
2023-11-27 22:57:08 -0800

    Add files via upload

--- userpopout.js
+++ userpopout.js
@@ -1,13 +1,28 @@
-function checkLogin() {
-    const loggedIn = localStorage.getItem('loggedIn');
-
-    if (loggedIn === 'true') {
-        alert('User is logged in!');
-    } else {
-        alert('User is not logged in.');
-    }
+var titlesVisible = false;
+function handleClientLoad() {
+    gapi.load('auth2', initSignIn);
 }
-document.addEventListener('DOMContentLoaded', function() {
-    // Call the function to handle login state when the page loads
-    handleLoginState();
-});
+
+function toggleTitles() {
+    console.log('Toggle titles function called');
+
+    var isUserSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+
+    if( isUserSignedIn ) {
+    var titles = document.querySelectorAll('.header .title.hidden');
+    titles.forEach(function (title) {
+        if (titlesVisible) {
+            title.style.display = 'none';
+        } else {
+            title.style.display = 'block';
+        }
+    });
+
+    titlesVisible = !titlesVisible;
+} else {
+    window.location.href = 'login.html';
+}
+}
+
+handleClientLoad();
+document.getElementById('userCircle').addEventListener('click', toggleTitles);

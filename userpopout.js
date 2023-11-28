1c74caa582e4bf3faef9c145ad88b131eabf31f4
Bleepblop69 <75205610+Bleepblop69@users.noreply.github.com>
2023-11-28 11:17:10 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -1,32 +1,81 @@
-function checkbutton() {
-    // Retrieve the 'loggedIn' value from localStorage
-    const loggedIn = localStorage.getItem('loggedIn');
-    
-    // Check if the user is logged in
-    if (loggedIn === 'true') {
-        alert('User is logged in!');
 
-        // Get all elements with the class "title-hidden"
-        const hiddenTitles = document.querySelectorAll('.title-hidden');
 
-        // Change the display status of each hidden title to "block"
-        hiddenTitles.forEach(title => {
-            title.style.display = 'block';
-        });
+If you want the options to appear when the user clicks the "user button" and then disappear if the user clicks anywhere else on the page, you can use an event listener on the document body to detect clicks outside the "user button" and options. Here's an updated example:
 
-        // Add a click event listener to the document
-        document.addEventListener('click', function hideTitles() {
-            // Change the display status of each hidden title to "none" when any click occurs
-            hiddenTitles.forEach(title => {
-                title.style.display = 'none';
-            });
+html
+Copy code
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <style>
+        .title-hidden {
+            display: none;
+        }
+    </style>
+</head>
+<body>
 
-            // Remove the click event listener after it's triggered (optional, depending on your needs)
-            document.removeEventListener('click', hideTitles);
-        });
-    } else {
-        // If the user is not logged in, display an alert and redirect to the login page
-        alert('User is not logged in.');
-        window.location.href = 'login.html';
-    }
-}
+    <!-- Your HTML content here -->
+
+    <button id="userButton">User Button</button>
+    <div class="option1 title-hidden">Option 1</div>
+    <div class="option2 title-hidden">Option 2</div>
+
+    <script>
+        function checkbutton() {
+            // Retrieve the 'loggedIn' value from localStorage
+            const loggedIn = localStorage.getItem('loggedIn');
+
+            // Get the user button, and options
+            const userButton = document.getElementById('userButton');
+            const option1 = document.querySelector('.option1');
+            const option2 = document.querySelector('.option2');
+
+            // Function to show options
+            function showOptions() {
+                option1.style.display = 'block';
+                option2.style.display = 'block';
+            }
+
+            // Function to hide options
+            function hideOptions() {
+                option1.style.display = 'none';
+                option2.style.display = 'none';
+            }
+
+            // Check if the user is logged in
+            if (loggedIn === 'true') {
+                // If logged in, show additional options
+                showOptions();
+
+                // Add a click event listener to the user button
+                userButton.addEventListener('click', function (event) {
+                    // Toggle options visibility when the user button is clicked
+                    if (option1.style.display === 'none' && option2.style.display === 'none') {
+                        showOptions();
+                    } else {
+                        hideOptions();
+                    }
+
+                    // Prevent the click event from propagating to the document
+                    event.stopPropagation();
+                });
+
+                // Add a click event listener to the document body
+                document.body.addEventListener('click', function hideOptionsOutside() {
+                    // Hide the options when the user clicks anywhere else on the page
+                    hideOptions();
+                });
+            } else {
+                // If not logged in, redirect to the login page when the user button is clicked
+                userButton.addEventListener('click', function redirectToLogin() {
+                    window.location.href = 'login.html';
+                });
+            }
+        }
+
+        // Call the function when the page loads
+        checkbutton();

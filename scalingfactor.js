d048303410f0f8a099ab1e878f5bd726054bc178
kbhypno <101908689+kbhypno@users.noreply.github.com>
2023-11-27 19:22:46 -0800

    Add files via upload

--- scalingfactor.js
+++ scalingfactor.js
@@ -1,4 +1,15 @@
-var screenWidth = window.screen.width;
-var baseResolutionWidth = 1920;
-var scalingFactor = screenWidth / baseResolutionWidth;
-document.documentElement.style.setProperty('--scaling-factor', scalingFactor);
+        // Get the user's screen width
+        function handleResize() {
+            var screenWidth = window.innerWidth;
+
+            // Set the base resolution width
+            var baseResolutionWidth = 1920;
+
+            // Calculate the scaling factor
+            var scalingFactor = screenWidth / baseResolutionWidth;
+
+            // Set the scaling factor as a CSS variable
+            document.documentElement.style.setProperty('--scaling-factor', scalingFactor);
+            }
+            window.onresize =handleResize;
+            onload = handleResize;

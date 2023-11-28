146d041df0c76cd8a542cd0b48b5696094cb7c46
Bleepblop69 <75205610+Bleepblop69@users.noreply.github.com>
2023-11-28 00:20:36 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -3,6 +3,14 @@ function checkbutton() {
     
     if (loggedIn === 'true') {
         alert('User is logged in!');
+
+        // Get all elements with the class "title-hidden"
+        const hiddenTitles = document.querySelectorAll('.title-hidden');
+
+        // Change the display status of each hidden title to "block"
+        hiddenTitles.forEach(title => {
+            title.style.display = 'block';
+        });
     } else {
         alert('User is not logged in.');
         window.location.href = 'login.html';

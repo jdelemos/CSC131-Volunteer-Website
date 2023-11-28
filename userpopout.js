3c8e53f3b962f6ba69def75602fc6506c621db1a
kbhypno <101908689+kbhypno@users.noreply.github.com>
2023-11-27 23:08:48 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -2,14 +2,23 @@ function checkbutton() {
     const loggedIn = localStorage.getItem('loggedIn');
     
     if (loggedIn === 'true') {
-        alert('User is logged in!');
+    var titles = document.querySelectorAll('.header .title.hidden');    
+    titles.forEach(function (title) {
+       if (titlesVisible) {
+            title.style.display = 'none';
+        } else {
+            title.style.display = 'block';
+        });
     } else {
-        alert('User is not logged in.');
+        window.location.href = 'login.html';
     }
 }
 
+document.getElementById('userCircle').addEventListener('click', toggleTitles);
 // Ensure that handleLoginState is called after the document has fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     // Call the function to handle login state when the page loads
     handleLoginState();
 });
+
+

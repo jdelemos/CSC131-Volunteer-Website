75af9c376ce347a959d768f69e1bcaf558fdfaa1
kbhypno <101908689+kbhypno@users.noreply.github.com>
2023-11-27 22:41:58 -0800

    Update userpopout.js

--- userpopout.js
+++ userpopout.js
@@ -1,34 +1,23 @@
-//var titlesVisible = false;
+var titlesVisible = false;
 
+function checkLogin(){
+    var titles = document.querySelectorAll('.header .title.hidden');
 
- //   var titles = document.querySelectorAll('.header .title.hidden');
-  //  titles.forEach(function (title) {
-   //     if (titlesVisible) {
-    //        title.style.display = 'none';
-    //    } else {
-    //        title.style.display = 'block';
-    //    }
-   // });
-
-   // titlesVisible = !titlesVisible;
-//} else {
-//    window.location.href = 'login.html';
-//}
-//}
-
-//document.getElementById('userCircle').addEventListener('click', toggleTitles);
-
-function checkLogin() {
-    const loggedIn = localStorage.getItem('loggedIn');
-
-    if (loggedIn === 'true') {
-        alert('User is logged in!');
-    } else {
-        alert('User is not logged in.');
+    if(loggedin){
+    titles.forEach(function (title) {
+        if (titlesVisible) {
+            title.style.display = 'none';
+        } else {
+            title.style.display = 'block';
+        }
+    });
     }
+    titlesVisible = !titlesVisible;
+} else {
+    window.location.href = 'login.html';
 }
+handleLoginState();
+}
+
+document.getElementById('userCircle').addEventListener('click', toggleTitles);
 document.addEventListener('DOMContentLoaded', function() {
-    // Call the function to handle login state when the page loads
-    handleLoginState();
-});
-</script>

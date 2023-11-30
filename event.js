e486fa7dbc9c93771d04e7adaefc3cfdb04f5282
kbhypno <101908689+kbhypno@users.noreply.github.com>
2023-11-29 18:10:04 -0800

    Update event.js

--- event.js
+++ event.js
@@ -19,6 +19,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Function that uses word wrap for the description on events after a certain amount of characters
+function wrapText(text, maxLineLength) {
+    let wrappedText = '';
+    let lineLength = 0;
+
+    text.split(' ').forEach(word => {
+        if (lineLength + word.length > maxLineLength) {
+            wrappedText += '\n';
+            lineLength = 0;
+        }
+        wrappedText += word + ' ';
+        lineLength += word.length + 1;
+    });
+
+    return wrappedText;
+}
+
+document.getElementById('description').textContent = wrapText(item.description, 84);
+
 async function run() {
   try {
     // Connect the client to the server

1470ba114e419718fd78b2cd67cb6eaece2ed835
Bleepblop69 <75205610+Bleepblop69@users.noreply.github.com>
2023-11-30 20:07:38 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,31 +1,8 @@
 function signIn(){
-    let oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth"
+        let serverAuthEndpoint = "https://vast-wave-12355-e83778ef23ea.herokuapp.comauth/google";
 
-    let form = document.createElement('form')
-    form.setAttribute('method','GET')
-    form.setAttribute('action',oauth2Endpoint)
-
-    let params = {
-        "client_id": "725527432824-s08ru0mm5mfaf0omp6i56n3d8k6j32u7.apps.googleusercontent.com",
-        "redirect_uri":"https://jdelemos.github.io/VW/index.html",
-        "response_type":"token",
-        "scope": "https://www.googleapis.com/auth/userinfo.profile ",
-        "include_granted_scopes":true,
-        'state':'pass-through-value'
-    }
-
-    for(var p in params){
-        let input = document.createElement('input')
-        input.setAttribute('type', 'hidden')
-        input.setAttribute('name', p)
-        input.setAttribute('value',params[p])
-        form.appendChild(input)
-    }
-
-    document.body.appendChild(form)
-
-    form.submit()
-}
+    // Redirect the user to your server's authentication endpoint
+    window.location.href = serverAuthEndpoint;
 function signOut() {
     // Remove the user's session data from localStorage
     localStorage.removeItem('loggedIn');
